82e3c983d06bb9d2eec635ca66b4e0c8edf9540a
youa7878 <dkgus7123@naver.com>
2023-02-09 02:18:04 +0900

    🔨  Add or update development scripts.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <>
+      <div>안녕하세요</div>
+      <div>반갑습니다</div>
+    </>
+  );
+};
 
 export default App;
