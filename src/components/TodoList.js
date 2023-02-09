f0554923684ba94b4b32c18180dd9ac70accaaec
youa7878 <dkgus7123@naver.com>
2023-02-09 15:45:30 +0900

    🔨  TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const TodoListBlock = styled.div``;
+const TodoListBlock = styled.div`
+  flex: 1;
+  background-color: #ffcdd2;
+  overflow-y: auto;
+`;
 
 const TodoList = () => {
   return <TodoListBlock>TodoList</TodoListBlock>;
