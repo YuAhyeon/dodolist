c6a7d6617a8f441ca928b82e606e0fd49859cee9
youa7878 <dkgus7123@naver.com>
2023-02-09 15:40:58 +0900

    🐛  Fix a bug

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -30,15 +30,15 @@ const TodoHeadBlock = styled.div`
 `;
 
 const TodoHead = () => {
-  const week = new Array(
+  const week = [
     '일요일',
     '월요일',
     '화요일',
     '수요일',
     '목요일',
     '금요일',
-    '토요일'
-  );
+    '토요일',
+  ];
   const getToday = new Date();
   const getYear = getToday.getFullYear();
   const getMonth = getToday.getMonth() + 1;
---
+++ src/components/TodoList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoListBlock = styled.div``;
+
+const TodoList = () => {
+  return <TodoListBlock>TodoList</TodoListBlock>;
+};
+
+export default TodoList;
